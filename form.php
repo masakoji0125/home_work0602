4db92ad444c01d6ff6d21cbef07dd534882235c7
具志堅雅子 <amour.vous.0125@gmail.com>
2020-06-03 22:39:50 +0900

    修正予定

--- form.php
+++ form.php
@@ -1,6 +1,13 @@
 
 <?php
 
+function h ($str) {
+   
+    $result = htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+
+    return $result;
+}
+
 $name = $_POST['name'];
 echo $name;
 
@@ -19,8 +26,19 @@ echo $comment;
 </head>
 <body>
     <h1>入力内容確認</h1>
+    
+    <p>お名前：<?= h ($name); ?></p>
+    <p>お問い合わせ内容：<?= h ($comment); ?> </p>
 
-    <p>お名前：<?= $name; ?></p>
-    <p>お問い合わせ内容：<?= $comment; ?> </p>
+    <form action="thanks.php" method ="POST">
+    <input type="hidden" name="name" value="<?=  h ($name); ?>">
+    <input type="hidden" name="comment" value="<?= h($comment); ?>">
+
+    <button type ="button" onclick="history.back()">戻る</button>
+
+    <?php if ($name != '' && $comment != '') { ?>
+    <button type="submit">確定</button>
+    <?php } ?>
+    </form>
 </body>
 </html>
